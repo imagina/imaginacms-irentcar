479a3c2f95be9909d0fe5c4389c95b72ba5127ed
William <williamverde10@gmail.com>
2025-10-22 10:55:50 -0400

    Update: added telephone to email reservation | Delete: age in email reservation

--- resources/views/emails/reservation/partials/user-infor.blade.php
+++ resources/views/emails/reservation/partials/user-infor.blade.php
@@ -6,7 +6,7 @@
   <h2 style="font-size: 22px; margin-bottom: 25px; color: #212529;">
     {{itrans('irentcar::email.user information')}}
   </h2>
-  
+
   <p style="margin:15px 0 0; color: #212529;font-size: 17px;"><strong>{{itrans('irentcar::email.name')}}</strong>
   </p>
   <div style="font-size:14px; color: #555; margin-bottom: 5px">
@@ -19,23 +19,23 @@
   <div style="font-size:14px; color: #555; margin-bottom: 5px">
     {{ $user->email }}
   </div>
-  
+
+  {{--
   <p style="margin:15px 0 0; color: #212529;font-size: 17px;">
     <strong>{{itrans('iuser::users.fields.age')}}: </strong>
   </p>
   <div style="font-size:14px; color: #555; margin-bottom: 5px">
     {{$user->age}}
   </div>
-  
-  {{--
-  @foreach ($fields as $field)
-  <p style="margin: 0; color: #000;"><strong>{{$field->title}}: </strong></p>
-  <div style="font-size:14px; color: #5B6077; margin-bottom: 5px">
-      {{$field->value}}
-  </div>
-  @endforeach
   --}}
-  
+
+  <p style="margin:15px 0 0; color: #212529;font-size: 17px;">
+    <strong>{{itrans('iuser::users.fields.telephone')}}: </strong>
+  </p>
+  <div style="font-size:14px; color: #555; margin-bottom: 5px">
+    {{$user->telephone}}
+  </div>
+
   <hr style="background-color: transparent;border: 0.5px solid #e2e2e2; margin:40px 0;">
 
 </div>
