1b9e1c44992146f06a569e3d890ac6965d450d54
William <williamverde10@gmail.com>
2025-10-22 11:39:38 -0400

    Added: extra validation

--- app/Http/Controllers/Api/ReservationApiController.php
+++ app/Http/Controllers/Api/ReservationApiController.php
@@ -109,7 +109,8 @@ class ReservationApiController extends CoreApiController
       $dataToSave = $reservationService->getDataToCreate($modelData, true);
 
       //Add conversion to USD
-      $dataToSave['gamma_office_extra_total_price_conversions'] = PriceHelper::getPriceConversions($dataToSave['gamma_office_extra_total_price'], $dataToSave['options']);
+      if (isset($dataToSave['gamma_office_extra_total_price']))
+        $dataToSave['gamma_office_extra_total_price_conversions'] = PriceHelper::getPriceConversions($dataToSave['gamma_office_extra_total_price'], $dataToSave['options']);
       $dataToSave['gamma_office_price_conversions'] = PriceHelper::getPriceConversions($dataToSave['gamma_office_price'], $dataToSave['options']);
       $dataToSave['total_price_conversions'] = PriceHelper::getPriceConversions($dataToSave['total_price'], $dataToSave['options']);
 
